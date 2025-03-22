3ed80819c67526cdcf039503501f15b30ee2a1e0
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-03-22 13:24:29 +0300

    fix: adjust index.js for Vercel serverless

--- config/client.js
+++ config/client.js
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { execSync } from 'child_process';
 
+try {
+  execSync('npx prisma generate');
+} catch (err) {
+  console.error('Failed to generate Prisma Client:', err);
+}
 const prisma = new PrismaClient();
 
 export default prisma;
