0debb15c74603dadce0ad2d117500a471360fcc7
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-03-22 13:47:55 +0300

    fix: adjust index.js for Vercel serverless

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,7 @@ import express from "express"
 const userRouter= express.Router()
 const userController= new UserController()
 userRouter.get("/getall-user",//verifyToken(true)
-,userController.getUsers.bind(userController))
+userController.getUsers.bind(userController))
 userRouter.get("/getone-user/:id",verifyToken(true),userController.getOneUser.bind(userController))
 userRouter.put("/update-user/:id",verifyToken(),userController.updateUser.bind(userController))
 userRouter.delete("/delete-user/:id",verifyToken(true),userController.deleteUser.bind(userController))
