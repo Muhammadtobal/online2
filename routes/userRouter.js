fb12ebcfaa07c9f86f810896f56b7205d4812c9e
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-03-22 13:45:26 +0300

    fix: adjust index.js for Vercel serverless

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,8 @@ import verifyToken from "../middlewares/identifaction.js";
 import express from "express"
 const userRouter= express.Router()
 const userController= new UserController()
-userRouter.get("/getall-user",verifyToken(true),userController.getUsers.bind(userController))
+userRouter.get("/getall-user",//verifyToken(true)
+,userController.getUsers.bind(userController))
 userRouter.get("/getone-user/:id",verifyToken(true),userController.getOneUser.bind(userController))
 userRouter.put("/update-user/:id",verifyToken(),userController.updateUser.bind(userController))
 userRouter.delete("/delete-user/:id",verifyToken(true),userController.deleteUser.bind(userController))
